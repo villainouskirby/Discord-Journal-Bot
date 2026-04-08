5227fa2083f4c1efeadcc6cb548a1bf42a91c68c
villainouskirby <69393156+villainouskirby@users.noreply.github.com>
2026-04-09 03:58:47 +0900

    벌금 현황 정산 기능 추가

--- discord_bot.py
+++ discord_bot.py
@@ -1,11 +1,13 @@
 import discord
 from discord import app_commands
-from datetime import datetime, timezone, timedelta
 import asyncio
 
 import os
 import aiohttp
 from aiohttp import web
+
+from collections import defaultdict
+from datetime import datetime, timezone, timedelta
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
@@ -14,15 +16,21 @@ tree = app_commands.CommandTree(client)
 try:
     forum_channel_id = int(os.environ['FORUM'])
     fine_channel_id = int(os.environ['FINE'])
+    fine_stat_channel_id = int(os.environ["FINE_STAT"])
 
 except:
     forum_channel_id = 0
     fine_channel_id = 0
+    fine_stat_channel_id = 0
 
 forum_channel = None
 fine_channel = None
+fine_stat_channel = None
 last_created_post = None
+last_created_fine = None
 recent_msg = None
+
+KST = timezone(timedelta(hours=9))
 
 ##########
 # 클래스
@@ -73,7 +81,7 @@ class CheckView(discord.ui.View):
 # 호스팅 메서드
 
 def now_kst():
-    return datetime.now(timezone.utc) + timedelta(hours=9)
+    return datetime.now(KST)
 
 async def health_check(request):
     return web.Response(text="OK", status=200)
@@ -119,17 +127,7 @@ def parse_users(text: str):
 
 ## 가장 최근에 생성된 포스트 가져오기
 async def get_latest_post():
-    forum = None
-    
-    try:
-        forum = await client.fetch_channel(forum_channel_id)
-        
-    finally:
-        if forum == None or not isinstance(forum, discord.ForumChannel):
-            print("ID Error! This is not Forum Channel.")
-            return None
-    
-    threads = forum.threads
+    threads = forum_channel.threads
     
     if not threads:
         return None
@@ -229,7 +227,40 @@ async def settlefine(thread: discord.Thread):
     
     await fine_channel.send(text)
     
-    return True
+    return text
+
+## 벌금 현황 텍스트 반환
+def get_fine_stat_text(dict: defaultdict):
+    txt = "벌금 현황\n\n"
+    
+    for mem in client.guilds[0].members:
+        if mem.bot:
+            continue
+        
+        txt += f"{mem.mention} : {dict[mem.mention]}회\n"
+        
+    return txt
+
+## 벌금 현황 업데이트
+async def update_fine_stat(fine_text: str):
+    global last_created_fine
+    
+    if last_created_fine == None:
+        return
+    
+    fine_stat_dict = defaultdict(int)
+    
+    for txt in last_created_fine.content.split("\n")[1:]:
+        mem, _, num = txt.split()
+        num = int(num[:-1])
+        
+        fine_stat_dict[mem] = num
+    
+    for mem in fine_text.split("\n")[1:]:
+        fine_stat_dict[mem] += 1
+    
+    txt = get_fine_stat_text(fine_stat_dict)
+    last_created_fine = await last_created_fine.edit(content=txt)
 
 ## 포럼 채널 ID 설정(관리자 명령어)
 @tree.command(name="setforum", description="포럼 채널 설정")
@@ -245,8 +276,8 @@ async def setforum(interaction: discord.Interaction, channel: discord.ForumChann
         ephemeral=True
     )
     
-## 벌금이력 채널 ID 설정(관리자 명령어)
-@tree.command(name="setfine", description="벌금이력 채널 설정")
+## 벌금 이력 채널 ID 설정(관리자 명령어)
+@tree.command(name="setfine", description="벌금 이력 채널 설정")
 @app_commands.checks.has_permissions(administrator=True)
 async def setfine(interaction: discord.Interaction, channel: discord.TextChannel):
     global fine_channel, fine_channel_id
@@ -277,8 +308,8 @@ async def createpost(interaction: discord.Interaction):
             ephemeral=True
         )
 
-## 벌금이력 강제 생성(관리자 명령어)
-@tree.command(name="createfine", description="벌금이력 생성")
+## 벌금 이력 강제 생성(관리자 명령어)
+@tree.command(name="createfine", description="벌금 이력 생성")
 @app_commands.checks.has_permissions(administrator=True)
 async def createfine(interaction: discord.Interaction):
     success = await settlefine(interaction.channel)
@@ -322,8 +353,8 @@ async def modifylist(interaction: discord.Interaction, member: discord.Member, v
         ephemeral=True
     )
 
-## 벌금이력 수정(관리자 명령어)
-@tree.command(name="modifyfine", description="벌금이력 수정")
+## 벌금 이력 수정(관리자 명령어)
+@tree.command(name="modifyfine", description="벌금 이력 수정")
 @app_commands.checks.has_permissions(administrator=True)
 async def modifyfine(interaction: discord.Interaction, messageid: str):
     if forum_channel == None or fine_channel == None:
@@ -340,7 +371,6 @@ async def modifyfine(interaction: discord.Interaction, messageid: str):
 
     try:
         msg = await fine_channel.fetch_message(int(messageid))
-        
     except:
         await interaction.response.send_message(
             "메시지를 찾을 수 없습니다.",
@@ -366,6 +396,74 @@ async def modifyfine(interaction: discord.Interaction, messageid: str):
         ephemeral=True
     )
 
+## 벌금 현황 생성(관리자 명령어)
+@tree.command(name="createfinestat", description="벌금 현황 생성")
+@app_commands.checks.has_permissions(administrator=True)
+async def createfinestat(interaction: discord.Interaction):
+    global last_created_fine
+    
+    if fine_stat_channel == None:
+        await interaction.response.send_message(
+            "채널 없음.",
+            ephemeral=True
+        )
+        
+        return
+
+    try:
+        last_created_fine = await fine_stat_channel.send(get_fine_stat_text(defaultdict(int)))
+        await interaction.response.send_message(
+            "벌금 현황 생성 완료",
+            ephemeral=True
+        )
+        
+    except:
+        await interaction.response.send_message(
+            "벌금 현황 생성 실패",
+            ephemeral=True
+        )
+
+## 벌금 현황 생성(관리자 명령어)
+@tree.command(name="modifyfinestat", description="벌금 현황 수정")
+@app_commands.checks.has_permissions(administrator=True)
+async def modifyfinestat(interaction: discord.Interaction, start_message_id: str = ""):
+    global last_created_fine
+    
+    start_msg = None
+    fine_stat_dict = defaultdict(int)
+    
+    if start_message_id != "":
+        start_msg = await fine_channel.fetch_message(start_message_id)
+        
+        if "벌금 대상" in start_msg.content and "없음" not in start_msg.content:
+            for user in start_msg.mentions:
+                fine_stat_dict[user.mention] += 1
+    
+    async for msg in fine_channel.history(after=start_msg):
+        if "벌금 대상" not in msg.content:
+            continue
+
+        if "없음" in msg.content:
+            continue
+        
+        for user in msg.mentions:
+            fine_stat_dict[user.mention] += 1
+    
+    txt = get_fine_stat_text(fine_stat_dict)
+    try:
+        last_created_fine = await last_created_fine.edit(content=txt)
+        await interaction.response.send_message(
+            "벌금 현황 수정 완료",
+            ephemeral=True
+        )
+        
+    except:
+        await interaction.response.send_message(
+            "벌금 현황 수정 실패",
+            ephemeral=True
+        )
+    
+    
 ## 00시 초기화 루프
 async def daily_check_loop():
     last_created_date = last_created_post.name if last_created_post else None
@@ -375,7 +473,7 @@ async def daily_check_loop():
         date_str = time.strftime("%Y-%m-%d")
 
         if last_created_date != date_str and time.hour == 0:
-            await settlefine(last_created_post)
+            await update_fine_stat(await settlefine(last_created_post))
             await create_daily_post(time)
             last_created_date = date_str
         
@@ -384,13 +482,14 @@ async def daily_check_loop():
 # 봇 로그인
 @client.event
 async def on_ready():
-    global forum_channel, fine_channel, last_created_post, recent_msg
+    global forum_channel, fine_channel, fine_stat_channel, last_created_post, last_created_fine, recent_msg
     
     print(f"Logged in as {client.user.name}")
     
     try:
         forum_channel = await client.fetch_channel(forum_channel_id)
         fine_channel = await client.fetch_channel(fine_channel_id)
+        fine_stat_channel = await client.fetch_channel(fine_stat_channel_id)
         
     finally:
         if forum_channel == None or not isinstance(forum_channel, discord.ForumChannel):
@@ -401,9 +500,16 @@ async def on_ready():
             print("ID Error! This is not Text Channel.")
             return None
         
+        if fine_stat_channel == None or not isinstance(fine_stat_channel, discord.TextChannel):
+            print("ID Error! This is not Text Channel.")
+            return None
+        
     last_created_post = await get_latest_post()
     if last_created_post != None:
         recent_msg = await last_created_post.fetch_message(last_created_post.id)
+    
+    msgs = [msg async for msg in fine_stat_channel.history(limit=1)]
+    last_created_fine = msgs[0] if msgs else None
     
     client.add_view(CheckView())
     await tree.sync()
